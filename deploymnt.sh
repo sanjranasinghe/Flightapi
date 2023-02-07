75e970be067a1a6d464848f123fddc517fd8913c
sanjaya ranasinghe <37567832+sanjranasinghe@users.noreply.github.com>
2023-02-07 18:14:50 +0530

    Create deploymnt.sh

---
+++ deploymnt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+AWS_CONFIG_FILE="~/.aws/config"
+
+n=4;#the variable that I want to be incremented
+next_n=$[$n+3]
+sed -i "/#the variable that I want to be incremented$/s/=.*#/=$next_n;#/" ${0}
+echo $next_n
+
+
+#cd /root/ansible_flight
+ansible-playbook ec2.yml --extra-vars "script=$1"
+sleep 1m
+ansible-playbook ec2_ami.yml --extra-vars "version=$next_n"
